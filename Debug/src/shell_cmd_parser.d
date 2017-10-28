5549185e207a00ff77b9c595277ce8719a4e3849
soierr <blackfin@ukr.net>
2017-10-28 20:24:43 +0300

    gitignore

--- Debug/src/shell_cmd_parser.d
+++
@@ -1,4 +0,0 @@
-src/shell_cmd_parser.o: ../src/shell_cmd_parser.c \
- ../src/shell_cmd_parser.h
-
-../src/shell_cmd_parser.h:
--- Debug/src/sniffer-demo.d
+++
@@ -1,6 +0,0 @@
-src/sniffer-demo.o: ../src/sniffer-demo.c ../src/shell_cmd_parser.h \
- ../src/shell_cmd_executor.h
-
-../src/shell_cmd_parser.h:
-
-../src/shell_cmd_executor.h:
