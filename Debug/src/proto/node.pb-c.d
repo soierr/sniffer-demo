8b3281942d1b8e67237062204c9b497ee9e5f7f0
soierr <blackfin@ukr.net>
2017-10-28 21:02:07 +0300

    d

--- Debug/src/proto/node.pb-c.d
+++
@@ -1,4 +0,0 @@
-src/proto/node.pb-c.o: ../src/proto/node.pb-c.c \
- /home/soierr/eclipse-workspace/sniffer-demo/src/proto/node.pb-c.h
-
-/home/soierr/eclipse-workspace/sniffer-demo/src/proto/node.pb-c.h:
